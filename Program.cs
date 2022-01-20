8dff3e6028b0c1aae5bfbe54d523c76979acd917
tomrosso <trosso2024@chadwickschool.org>
2022-01-20 11:02:59 -0800

    While loop

--- Program.cs
+++ Program.cs
@@ -6,21 +6,15 @@ namespace trosso2024_DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
-           int shift;
-           shift = 1;
-           while(shift <= 10){
-              Cipher cipher1;
-            cipher1 = new Cipher(1);
-            Console.WriteLine ((char)(shift + 1));
-           }
 
 
-           string filePath = args[0];
-           Console.WriteLine($"Loading '{filePath}'.");
-           
-           string message;
-           message = System.IO.File.ReadAllText(filePath);
-           Console.WriteLine($"The encrypted message is: {message}");
+
+            string filePath = args[0];
+            Console.WriteLine($"Loading '{filePath}'.");
+
+            string message;
+            message = System.IO.File.ReadAllText(filePath);
+            Console.WriteLine($"The encrypted message is: {message}");
 
             Cipher cipher;
             cipher = new Cipher(3);
@@ -29,9 +23,19 @@ namespace trosso2024_DataEncodingInCSharp
             //encrypted = cipher.Encrypt("rosebud");
             //Console.WriteLine($"The encrypted message is: '{encrypted}");
 
-           string decrypted;
-            decrypted = cipher.Decrypt(message);
-            Console.WriteLine($"The decrypted message is: '{decrypted}'");
+            int shift;
+            shift = 1;
+            while (shift <= 10)
+            {
+                Cipher cipher1;
+                cipher1 = new Cipher(shift);
+                string decrypted;
+                decrypted = cipher1.Decrypt(message);
+                Console.WriteLine($"The decrypted message is: '{decrypted}'");
+                shift = shift + 1;
+            }
+
+
         }
     }
 }
